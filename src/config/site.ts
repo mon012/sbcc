6858542d3f89b35db47259e239a29219b0557a85
mon012 <mon012@gmail.com>
2026-07-25 00:12:10 +0700

    v1

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export const site = {
+  name: "SBCC Engineering & Supply",
+  legalName: "บริษัท เอสบีซีซี เอ็นจิเนียริ่ง แอนด์ ซัพพลาย จำกัด",
+  description:
+    "จำหน่ายอุปกรณ์วิศวกรรมและให้บริการติดตั้งโดยทีมงานมืออาชีพ พร้อมให้คำปรึกษาและเสนอราคาด่วน",
+  url: "https://sbcceng.co.th",
+  officeDisplay: "02-006-9488",
+  officeHref: "tel:020069488",
+  hotlineDisplay: "085-909-4543",
+  hotlineHref: "tel:0859094543",
+  hotlineSecondaryDisplay: "062-854-9595",
+  hotlineSecondaryHref: "tel:0628549595",
+  lineDisplay: "@sbcceng",
+  lineHref: "https://lin.ee/5CHMocW",
+  email: "sbccengineeringsupply@gmail.com",
+  quoteFormId: "f85f767f-083d-40e6-bbea-de9840271e78",
+} as const;
